f03fe7683c56b4afa5d3eb3aa78c57168b04ac48
Jose Mauricio <jmauricio.a.ferreira@gmail.com>
2020-09-16 19:38:47 -0300

    Criacao da funcao initAnimacaoScroll que anima a pagina ao descer o scroll

--- script.js
+++ script.js
@@ -68,3 +68,30 @@ function initScrollSuave() {
 }
 
 initScrollSuave();
+
+function initAnimacaoScroll() {
+		const sections = document.querySelectorAll('.js-scroll');
+
+	if(sections.length) {
+		const windowMetade = window.innerHeight * 0.6;
+
+		function animaScroll() {
+			sections.forEach((section) => {
+				const sectionTop = section.getBoundingClientRect().top;
+				const isSectionVisible = (sectionTop - windowMetade);
+				if(isSectionVisible <0) {
+					section.classList.add('ativo');
+				} else {
+					section.classList.remove('ativo');
+				}
+			});
+		}
+
+		animaScroll();
+
+		window.addEventListener('scroll', animaScroll);
+	}	
+
+}
+
+initAnimacaoScroll();
