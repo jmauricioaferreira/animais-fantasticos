2412ea0fe116599444d355d049145d1abde36e9b
Jose Mauricio <jmauricio.a.ferreira@gmail.com>
2020-10-03 16:39:50 -0300

    Criando anima-numeros

--- js/modules/animacao-scroll.js
+++ js/modules/animacao-scroll.js
@@ -11,7 +11,7 @@ export default function initAnimacaoScroll() {
 				const isSectionVisible = (sectionTop - windowMetade);
 				if (isSectionVisible < 0) {
 					section.classList.add('ativo');
-				} else {
+				} else if(section.classList.contains('ativo')) {
 					section.classList.remove('ativo');
 				}
 			});
